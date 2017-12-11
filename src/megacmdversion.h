c6098c9e035f51ae4b33bb78dd15e54a3c83863b
Pablo Martin <pm@mega.nz>
2017-12-11 18:44:14 +0100

    update version and changelog to 0.9.7
    added packages changelogs

--- src/megacmdversion.h
+++ src/megacmdversion.h
@@ -8,7 +8,7 @@
 #define MEGACMD_MINOR_VERSION 9
 #endif
 #ifndef MEGACMD_MICRO_VERSION
-#define MEGACMD_MICRO_VERSION 6
+#define MEGACMD_MICRO_VERSION 7
 #endif
 
 #ifndef MEGACMD_CODE_VERSION
@@ -16,24 +16,9 @@
 #endif
 
 const char * const megacmdchangelog =
-        "Added transfer resumption""\n"
-        "Added file versioning for modified files(webclient can browse them)""\n"
-        "Added time and size constrains for find""\n"
-        "Reformated sync output""\n"
-        "Added exclusions to syncs. Default: .* ~* desktop.ini Thumbs.db""\n"
-        "Fixed detection of invalid TIMEVAL when no unit specified""\n"
-        "Improved sync display format""\n"
-        "Fix some \"get\" cases with \"/\" involved in Windows""\n"
-        "Fix in email validation""\n"
-        "Fix segfault in userattr""\n"
-        "Added \"--in\" & \"--out\" to showpcr""\n"
-        "Added masterkey command to show master key""\n"
-        "All/None options in confirmation for deletion""\n"
-        "Added multi-transfer progress (one single progress bar)""\n"
-        "Fix mkdir loop""\n"
-        "Minor fixes and improvements in error management""\n"
-        "beautify whoami -l and added storage used by versions""\n"
-        "Changed name for server in windows to MEGAcmdServer.exe"
+        "Faster scanning of files""\n"
+        "Improved the management of deleted files""\n"
+        "Bug fixes and other minor improvements"
         ;
 
 
