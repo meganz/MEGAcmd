35c8cd8a69dda79362e6bc27b0297d5f85098957
mattw-mega <mattw@mega.co.nz>
2018-05-25 14:08:01 +1200

    correct spelling

--- build/SynologyNAS/toolkit/source/megacmdpkg/INFO.sh
+++ build/SynologyNAS/toolkit/source/megacmdpkg/INFO.sh
@@ -8,7 +8,7 @@ displayname="MEGAcmd"
 maintainer="Mega NZ"      
 arch="$(pkg_get_unified_platform)"
 description="MEGAcmd command line tool"
-min_os_ver="6.1-14715"
+os_min_ver="6.1-14715"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
 
