178bc915ee5fe1a301ee51491f99fef7a905c76e
mattw-mega <mattw@mega.co.nz>
2018-05-25 18:06:23 +1200

    added a few more links to MEGA in the INFO.sh

--- build/SynologyNAS/toolkit/source/megacmdpkg/INFO.sh
+++ build/SynologyNAS/toolkit/source/megacmdpkg/INFO.sh
@@ -9,6 +9,11 @@ maintainer="Mega NZ"
 arch="$(pkg_get_unified_platform)"
 description="MEGAcmd command line tool"
 os_min_ver="6.1-14715"
+maintainer="Mega Ltd."
+maintaner_url="https://MEGA.nz"
+support_url="https://mega.nz/help"
+helpurl="https://github.com/meganz/MEGAcmd/blob/master/UserGuide.md"
+thirdparty="yes"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
 
