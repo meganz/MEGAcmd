295de65706686f27ba869c0229e6a333ef7b1718
Alfonso Abella <aag@mega.co.nz>
2022-01-21 17:31:23 +0100

    Remove unneded dsc files

--- build/create_tarball.sh
+++ build/create_tarball.sh
@@ -55,15 +55,11 @@ rm -fr megacmd/megacmd*.dsc
 
 # fix version number in template files and copy to appropriate directories
 sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/megacmd.spec | sed "s#^ *##g" > megacmd/megacmd.spec
-#sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/megacmd.dsc > megacmd/megacmd.dsc
-for dist in xUbuntu_{1,2}{0,1,2,3,4,5,6,7,8,9}.{04,10} Debian_{7,8,9,10}.0 Debian_11 Debian_testing; do
-if [ -f templates/megacmd/megacmd-$dist.dsc ]; then
-    sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/megacmd-$dist.dsc > megacmd/megacmd-$dist.dsc
-else
-    sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/megacmd.dsc > megacmd/megacmd-$dist.dsc
-fi
-done
+sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/megacmd.dsc > megacmd/megacmd.dsc
 sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" templates/megacmd/PKGBUILD > megacmd/PKGBUILD
+for dscFile in `find templates/megacmd/ -name megacmd-xUbuntu_* -o -name megacmd-Debian_* -o -name megacmd-Raspbian_*`; do
+    sed -e "s/megacmd_VERSION/$megacmd_VERSION/g" "${dscFile}" > megacmd/`basename ${dscFile}`
+done
 
 # read the last generated ChangeLog version
 version_file="version"
