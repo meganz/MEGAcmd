788643334d0a6a1a2cebc6c08f1351e9234d18d8
Alfonso Abella <aag@mega.co.nz>
2022-06-16 16:13:57 +0200

    Remove unneded Qt plugins

--- build/installer_mac.sh
+++ build/installer_mac.sh
@@ -196,6 +196,10 @@ if [ ${build} -eq 1 -o ${build_cmake} -eq 1 ]; then
 
 
     if [ ${build_cmake} -ne 1 ]; then
+
+        # Remove unneded Qt plugins
+        rm -rf ${SERVER_PREFIX}MEGAcmd.app/Contents/Plugins
+
         [ ! -f MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/$AVCODEC_VERSION ] && cp -L $AVCODEC_PATH MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/
         [ ! -f MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/$AVFORMAT_VERSION ] && cp -L $AVFORMAT_PATH MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/
         [ ! -f MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/$AVUTIL_VERSION ] && cp -L $AVUTIL_PATH MEGAcmdServer/MEGAcmd.app/Contents/Frameworks/
