99affff4d00a495c449a9ae4963c978410fb2e85
Pablo Martin <pm@mega.nz>
2023-03-01 13:33:58 +0100

    update version files and changelogs to 1.6.0 and sdk to v4.16.0a

--- src/megacmdversion.h
+++ src/megacmdversion.h
@@ -5,10 +5,10 @@
 #define MEGACMD_MAJOR_VERSION 1
 #endif
 #ifndef MEGACMD_MINOR_VERSION
-#define MEGACMD_MINOR_VERSION 5
+#define MEGACMD_MINOR_VERSION 6
 #endif
 #ifndef MEGACMD_MICRO_VERSION
-#define MEGACMD_MICRO_VERSION 1
+#define MEGACMD_MICRO_VERSION 0
 #endif
 
 #ifndef MEGACMD_BUILD_ID
@@ -21,17 +21,12 @@
 namespace megacmd {
 
 const char * const megacmdchangelog =
-#ifdef __MACH__
-        "Add support for pdfs (uploading them will create thumbnails/previews)""\n"
-        "Improve communications with server in POSIX: no longer create multiple sockets""\n"
-        "Support resuming session when logged into a folder""\n"
-        "Renew and improve sync management and improve status reporting""\n"
-        "Commands sync and transfers now allow for selecting --output-cols""\n"
-        "Add creation time to ls""\n"
-        "Fix issue running from php (due to a dangling file descriptor)""\n"
-        "Fix several crashes, leaks and major performance improvements""\n"
-#endif
-        "Security adjustments"
+        "Improvements in \"find\" to search by type, or print only handles (to ease automation)""\n"
+        "Improved state management and reporting in synchronizations""\n"
+        "Improved information on shares""\n"
+        "Improvements in \"attr\"""\n"
+        "Security adjustments""\n"
+        "Other fixes and adjustments"
         ;
 
 }//end namespace
