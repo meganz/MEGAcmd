66b29a5a0181ccb9fca8409f908d21a31761d6b7
Xavier Rins Lozano <xrl@mega.co.nz>
2023-10-17 13:24:45 +0200

    add jenkinsfiles

---
+++ build-with-qt/builder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+volume_dest="/opt/megacmd"
+owner_uid=`stat -c "%u" $volume_dest`
+owner_gid=`stat -c "%g" $volume_dest`
+
+
+groupadd -g $owner_gid jenkins
+echo "Adding \"jenkins\" user..."
+useradd -r -M -u $owner_uid -g $owner_gid -d $volume_dest -s /bin/bash jenkins
+chown -R jenkins:jenkins $volume_dest
+
+echo "Configuring core dumpings on docker"
+ulimit -c unlimited
+
+rm -rf installdir || :
+su -c "mkdir installdir" - jenkins
+su -c "./autogen.sh" - jenkins
+su -c "./sdk/contrib/build_sdk.sh -b -g -f -I -i -s -n -z -o ./3rd_pkgs -p ./3rd_deps" - jenkins
+su -c "rm ./3rd_deps/include/sqlite* ./3rd_deps/lib/libsqlite* || :" - jenkins
+su -c "./configure --disable-silent-rules --disable-examples --prefix=$volume_dest/installdir --with-curl=$volume_dest/3rd_deps --disable-curl-checks" - jenkins
+
+su -c "make clean" - jenkins
+su -c "make" - jenkins
+su -c "make install" - jenkins
+
