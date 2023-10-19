a31bb780702ab2fb9ef28f5f6f45803cacdef2bc
Pablo Martin <pm@mega.nz>
2023-10-19 17:04:52 +0200

    compile gtests & run unit tests

--- build-with-docker/builder.sh
+++ build-with-docker/builder.sh
@@ -18,9 +18,12 @@ exec su - jenkins -c "
 ./autogen.sh
 ./sdk/contrib/build_sdk.sh -b -g -f -I -i -s -n -z -o ./3rd_pkgs -p ./3rd_deps
 rm ./3rd_deps/include/sqlite* ./3rd_deps/lib/libsqlite* || :
-./configure --disable-silent-rules --disable-examples --prefix=$volume_dest/installdir --with-curl=$volume_dest/3rd_deps --disable-curl-checks
+./configure --disable-silent-rules --disable-examples --prefix=$volume_dest/installdir --with-curl=$volume_dest/3rd_deps --disable-curl-checks --enable-megacmd-tests --with-gtest
 
 make clean
 make
 make install
+
+#now run the unit tests
+$volume_dest/installdir/mega-cmd-unit-tests
 "
