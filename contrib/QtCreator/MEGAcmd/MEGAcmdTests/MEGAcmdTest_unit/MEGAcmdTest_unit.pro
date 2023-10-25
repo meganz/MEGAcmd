da5d3028a16ea24136d9b45c6656c235351f32c8
Vibhav Pant <vpa@mega.co.nz>
2023-10-25 16:57:58 +0530

    Replace FileSocketCommunicationTests.cpp with PlatformDirectoriesTest.cpp

--- contrib/QtCreator/MEGAcmd/MEGAcmdTests/MEGAcmdTest_unit/MEGAcmdTest_unit.pro
+++ contrib/QtCreator/MEGAcmd/MEGAcmdTests/MEGAcmdTest_unit/MEGAcmdTest_unit.pro
@@ -62,7 +62,7 @@ INCLUDEPATH += \
 
 SOURCES += \
     $$MEGACMD_BASE_PATH/tests/unit/StringUtilsTests.cpp \
-    $$MEGACMD_BASE_PATH/tests/unit/FileSocketCommunicationTests.cpp \
+    $$MEGACMD_BASE_PATH/tests/unit/PlatformDirectoriesTest.cpp \
     $$MEGACMD_BASE_PATH/tests/unit/main.cpp
 
 #Dependencies:
---
+++ tests/unit/PlatformDirectoriesTest.cpp
@@ -0,0 +1,111 @@
+/**
+ * (c) 2013 by Mega Limited, Auckland, New Zealand
+ *
+ * This file is part of MEGAcmd.
+ *
+ * MEGAcmd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright Simplified (2-clause) BSD License.
+ *
+ * You should have received a copy of the license along with this
+ * program.
+ */
+
+#include <gtest/gtest.h>
+
+#include "Instruments.h"
+#include "TestUtils.h"
+#include "comunicationsmanagerfilesockets.h"
+#include "megacmdcommonutils.h"
+
+TEST(PlatformDirectoriesTest, runtimeDirPath)
+{
+    using megacmd::PlatformDirectories;
+
+    auto dirs = PlatformDirectories::getPlatformSpecificDirectories();
+    ASSERT_TRUE(dirs != nullptr);
+
+#if defined(__unix__) && !defined(__APPLE__)
+    {
+        G_SUBTEST << "With $XDG_RUNTIME_DIR";
+        auto guard = TestInstrumentsEnvVarGuard("XDG_RUNTIME_DIR", "/tmp/test");
+        EXPECT_EQ(dirs->runtimeDirPath(), "/tmp/test/megacmd");
+    }
+    {
+        G_SUBTEST << "Without $XDG_RUNTIME_DIR";
+        auto guard = TestInstrumentsEnvVarGuard("XDG_RUNTIME_DIR");
+        auto homeGuard = TestInstrumentsEnvVarGuard("HOME", "/home/test");
+        EXPECT_EQ(dirs->runtimeDirPath(),
+                  std::string("/tmp/megacmd-").append(std::to_string(getuid())));
+    }
+#endif
+}
+
+TEST(PlatformDirectoriesTest, configDirPath)
+{
+    using megacmd::PlatformDirectories;
+
+    auto dirs = PlatformDirectories::getPlatformSpecificDirectories();
+    ASSERT_TRUE(dirs != nullptr);
+#if defined(__unix__) && !defined(__APPLE__)
+    {
+        G_SUBTEST << "With $XDG_CONFIG_HOME";
+        auto homeGuard = TestInstrumentsEnvVarGuard("HOME", "/home/test");
+        auto guard = TestInstrumentsEnvVarGuard("XDG_CONFIG_HOME", "/tmp/test");
+        EXPECT_EQ(dirs->configDirPath(), "/tmp/test/megacmd");
+    }
+    {
+        G_SUBTEST << "Without $XDG_CONFIG_HOME";
+        auto guard = TestInstrumentsEnvVarGuard("XDG_CONFIG_HOME");
+        auto homeGuard= TestInstrumentsEnvVarGuard("HOME", "/home/test");
+        EXPECT_EQ(dirs->configDirPath(), "/home/test/.megaCmd");
+    }
+#endif    
+}
+
+TEST(PlatformDirectoriesTest, cacheDirPath)
+{
+    using megacmd::PlatformDirectories;
+
+    auto dirs = PlatformDirectories::getPlatformSpecificDirectories();
+    ASSERT_TRUE(dirs != nullptr);
+
+#if defined(__unix__) && !defined(__APPLE__)
+    {
+        G_SUBTEST << "With $XDG_CACHE_HOME";
+        auto homeGuard = TestInstrumentsEnvVarGuard("HOME", "/home/test");
+        auto guard = TestInstrumentsEnvVarGuard("XDG_CACHE_HOME", "/tmp/test");
+        EXPECT_EQ(dirs->cacheDirPath(), "/tmp/test/megacmd");
+    }
+    {
+        G_SUBTEST << "Without $XDG_CACHE_HOME";
+        auto homeGuard = TestInstrumentsEnvVarGuard("HOME", "/home/test");
+        auto guard = TestInstrumentsEnvVarGuard("XDG_CACHE_HOME");
+        EXPECT_EQ(dirs->cacheDirPath(), "/home/test/.megaCmd");
+    }
+#endif
+}
+
+#ifdef __unix__
+TEST(PlatformDirectoriesTest, getSocketPath)
+{
+    using megacmd::getSocketPath;
+    using megacmd::PlatformDirectories;
+
+    auto dirs = PlatformDirectories::getPlatformSpecificDirectories();
+    auto runtimeDir = dirs->runtimeDirPath();
+
+    {
+        G_SUBTEST << "With $MEGACMD_SOCKET_NAME";
+        auto guard = TestInstrumentsEnvVarGuard("MEGACMD_SOCKET_NAME", "megacmd-test.socket");
+        EXPECT_EQ(getSocketPath(false), runtimeDir + "/megacmd-test.socket");
+    }
+    {
+        G_SUBTEST << "With $MEGACMD_SOCKET_NAME";
+        auto guard = TestInstrumentsEnvVarGuard("MEGACMD_SOCKET_NAME");
+        EXPECT_EQ(getSocketPath(false), runtimeDir + "/megacmd.socket");
+    }
+}
+#endif
