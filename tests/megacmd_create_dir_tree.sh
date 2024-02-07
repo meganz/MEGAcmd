1d32eb57faec7c08ef6cd582ec25a635265d1696
Vibhav Pant <vpa@mega.co.nz>
2024-02-07 21:36:58 +0530

    Add files and folders for ExportTest.

--- tests/megacmd_create_dir_tree.sh
+++ tests/megacmd_create_dir_tree.sh
@@ -31,4 +31,9 @@ do
     done
 done
 
-mega-put $TMP/testReadingFolder01 /
+echo /file01.txt > $TMP/file01.txt
+
+mkdir -p $TMP/testExportFolder/subDirectoryExport
+echo /testExportFoldersubDirectoryExport/file01.txt > $TMP/testExportFolder/subDirectoryExport/file01.txt
+
+mega-put $TMP/ /
