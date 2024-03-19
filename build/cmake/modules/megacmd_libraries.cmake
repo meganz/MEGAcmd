90b5d8095a31c4051fc676da730e6f936ae71d18
Pablo Martin <pm@mega.nz>
2024-03-19 14:45:23 +0100

    initial version of new build system with cmake + vcpkg build

---
+++ build/cmake/modules/megacmd_libraries.cmake
@@ -0,0 +1,19 @@
+macro(load_megacmdserver_libraries)
+
+    if(VCPKG_ROOT)
+        if(USE_PCRE)
+            find_package(unofficial-pcre REQUIRED)
+            target_link_libraries(LMegacmdServer PRIVATE unofficial::pcre::pcrecpp)
+            set(USE_PCRE 1)
+        endif()
+    else() # No VCPKG usage. Use pkg-config
+        find_package(PkgConfig REQUIRED) # For libraries loaded using pkg-config
+
+        if(USE_PCRE) #TODO: UNTESTED!
+            pkg_check_modules(pcre REQUIRED IMPORTED_TARGET libpcre)
+            target_link_libraries(LMegacmdServer PRIVATE PkgConfig::pcre)
+            set(USE_PCRE 1)
+        endif()
+    endif()
+
+endmacro()
---
+++ build/cmake/modules/megacmd_options.cmake
@@ -0,0 +1,2 @@
+option(USE_PCRE "Used to provide support for pcre" ON)
+option(USE_LIBUV "Includes the library and turns on internal web and ftp server functionality" ON)
