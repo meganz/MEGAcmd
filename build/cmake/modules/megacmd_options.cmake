b638f19018f5dcfee0a231f04de589cf3810b1d1
Pablo Martin <pm@mega.nz>
2024-03-26 13:41:12 +0100

    enforce full requirements to new cmake build

--- build/cmake/modules/megacmd_options.cmake
+++ build/cmake/modules/megacmd_options.cmake
@@ -3,3 +3,14 @@ option(USE_LIBUV "Includes the library and turns on internal web and ftp server 
 
 #Backups:
 option(ENABLE_BACKUPS "Used to enable backups (scheduled copies)" ON)
+
+option(FULL_REQS "Fail compilation when some requirement is not met" ON)
+
+if(USE_PCRE)
+    add_compile_definitions(USE_PCRE) #this one is no longer added to target_compile_definitions(SDKlib, hence some cpps not using config.h will lack this, causing the full reqs to fail
+    # TODO: remove once PCRE optionality is removed
+endif()
+
+if(FULL_REQS)
+    add_compile_definitions(REQUIRE_HAVE_PDFIUM REQUIRE_HAVE_FFMPEG REQUIRE_HAVE_LIBUV REQUIRE_USE_MEDIAINFO REQUIRE_USE_PCRE)
+endif()
