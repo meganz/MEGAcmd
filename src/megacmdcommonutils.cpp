89cdb1b43b0449f2ffe7d8a5494f6d4dab08c535
Pablo Martin <pm@mega.nz>
2024-05-06 13:05:44 +0200

    prevent socket path to get too large due to runtime path location: fallback to /tmp
    - shorten socket name in macos tests, so as to use runtime path in tests

--- src/megacmdcommonutils.cpp
+++ src/megacmdcommonutils.cpp
@@ -25,6 +25,8 @@
 #include <Lmcons.h> //UNLEN
 #else
 #include <sys/ioctl.h> // console size
+#include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -1605,22 +1607,35 @@ std::string XDGDirectories::runtimeDirPath()
 std::string getOrCreateSocketPath(bool createDirectory)
 {
     auto dirs = PlatformDirectories::getPlatformSpecificDirectories();
-    auto runtimedir = dirs->runtimeDirPath();
-    if (runtimedir.empty())
+    auto socketFolder = dirs->runtimeDirPath();
+    if (socketFolder.empty())
     {
         return std::string();
     }
+
+    const char *sockname_c = getenv("MEGACMD_SOCKET_NAME");
+    std::string sockname = sockname_c != nullptr ? std::string(sockname_c) : "megacmd.socket";
+
+    static auto MAX_SOCKET_PATH = sizeof(sockaddr_un::sun_path) / sizeof(decltype(sockaddr_un::sun_path[0]));
+
+    if ((socketFolder.size() + 1 + sockname.size()) >= (MAX_SOCKET_PATH - 1))
+    {
+        std::cerr << "WARN: socket path in runtime dir would exceed max size. Falling back to /tmp" << std::endl;
+        socketFolder = "/tmp/megacmd";
+    }
+
+
     struct stat path_stat = {};
     if (createDirectory)
     {
-        bool exists = !stat(runtimedir.c_str(), &path_stat) && S_ISDIR(path_stat.st_mode);
+        bool exists = !stat(socketFolder.c_str(), &path_stat) && S_ISDIR(path_stat.st_mode);
         if (!exists && createDirectory)
         {
             mode_t mode = umask(0);
-            bool failed = mkdir(runtimedir.c_str(), 0700) != 0;
+            bool failed = mkdir(socketFolder.c_str(), 0700) != 0;
             if (failed)
             {
-                std::cerr << "Failed to create folder for unix socket: " << runtimedir << ": " << std::strerror(errno) << std::endl;
+                std::cerr << "Failed to create folder for unix socket: " << socketFolder << ": " << std::strerror(errno) << std::endl;
             }
             umask(mode);
 
@@ -1629,9 +1644,7 @@ std::string getOrCreateSocketPath(bool createDirectory)
         }
     }
 
-    const char *sockname_c = getenv("MEGACMD_SOCKET_NAME");
-    std::string sockname = sockname_c != nullptr ? std::string(sockname_c) : "megacmd.socket";
-    return runtimedir.append("/").append(sockname);
+    return socketFolder.append("/").append(sockname);
 }
 #endif // ifdef(_WIN32) else
 } //end namespace
