d38c7726697a6e40f07370343c63b676ee5b46ea
Pablo Martin <pm@mega.nz>
2024-05-15 14:39:42 +0200

    adjustments realted to vcpkg cloning
    - no longer clone it for macos MRs
    - reuse SDK's vcpkgs(expected in jenkins) in macos & windows
    - hardcode an older commit to look for in vcpkg cloning script (older
    than baseline, required after last changes in vcpkg.json)

--- build/clone_vcpkg_from_baseline.sh
+++ build/clone_vcpkg_from_baseline.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-commit=$(jq -r '."builtin-baseline"' vcpkg.json)
+#Currently libuv uses a git-tree older than baseline, enforcing older commit here:
+VCPKG_OLDEST_COMMIT=293f090a9be5a88a2a77838c6346d5ef9d3df81b
+
+if [ -z  "${VCPKG_OLDEST_COMMIT}" ]; then
+    commit=$(jq -r '."builtin-baseline"' vcpkg.json)
+else
+    commit="${VCPKG_OLDEST_COMMIT}"
+fi
 commit_date=$(curl -s "https://api.github.com/repos/microsoft/vcpkg/commits/$commit" | jq -r '.commit.author.date')
 git clone --shallow-since $commit_date https://github.com/microsoft/vcpkg $@
