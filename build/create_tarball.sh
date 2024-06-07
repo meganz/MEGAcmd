3aa283c733d9ed67ad5b59af97da10ddf2eb9992
Pablo Martin <pm@mega.nz>
2024-06-07 14:12:12 +0200

    temporarily include contrib in tarball

--- build/create_tarball.sh
+++ build/create_tarball.sh
@@ -87,7 +87,8 @@ ln -s ../megacmd/debian.prerm $megacmd_NAME/debian.prerm
 ln -s ../megacmd/debian.postrm $megacmd_NAME/debian.postrm
 ln -s ../megacmd/debian.copyright $megacmd_NAME/debian.copyright
 
-for i in $BASEPATH/{src,sdk,tests,CMakeLists.txt,vcpkg.json}; do
+#contrib/QtCreator (TODO: remove these from cmake and here)
+for i in $BASEPATH/{src,sdk,tests,CMakeLists.txt,vcpkg.json,contrib}; do
 	ln -s $i $megacmd_NAME/
 done
 
