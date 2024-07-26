216c8586bd4a9a716aacd5fccd1edcfeb33a9bf9
Diego Ximenez <dxi@mega.co.nz>
2024-07-26 15:25:51 +0200

    Remove files from legacy build systems

--- build/create_tarball.sh
+++ build/create_tarball.sh
@@ -87,8 +87,7 @@ ln -s ../megacmd/debian.prerm $megacmd_NAME/debian.prerm
 ln -s ../megacmd/debian.postrm $megacmd_NAME/debian.postrm
 ln -s ../megacmd/debian.copyright $megacmd_NAME/debian.copyright
 
-#contrib/QtCreator (TODO: remove these from cmake and here)
-for i in $BASEPATH/{src,sdk,tests,CMakeLists.txt,vcpkg.json,contrib}; do
+for i in $BASEPATH/{src,sdk,tests,CMakeLists.txt,vcpkg.json}; do
 	ln -s $i $megacmd_NAME/
 done
 
