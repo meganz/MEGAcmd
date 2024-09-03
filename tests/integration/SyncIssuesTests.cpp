49976e1c1ef22675366d1d7eb411e9c354cd817b
Diego Ximenez <dxi@mega.co.nz>
2024-09-03 11:32:56 +0200

    Use a shorter sync path for integration tests
    Otherwise, on Windows and macOS, the path was getting cut in the output
    of the sync command, making it hard to verify.

--- tests/integration/SyncIssuesTests.cpp
+++ tests/integration/SyncIssuesTests.cpp
@@ -94,12 +94,12 @@ class SyncIssuesTests : public NOINTERACTIVELoggedInTest
 protected:
     std::string syncDirLocal() const
     {
-        return mTmpDir.string() + "/tests_integration_sync_dir/";
+        return mTmpDir.string() + "/sync_dir/";
     }
 
     std::string syncDirCloud() const
     {
-        return "tests_integration_sync_dir/";
+        return "sync_dir/";
     }
 };
 
