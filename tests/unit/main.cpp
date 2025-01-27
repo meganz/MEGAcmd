f83b20b6f72dd5e52d2f22aaad87cda639948658
Pablo Martin <pm@mega.nz>
2025-01-27 17:27:15 +0100

    have gtests printing controlled also for unit tests

--- tests/unit/main.cpp
+++ tests/unit/main.cpp
@@ -18,6 +18,32 @@
 #include "megaapi.h"
 #include <gtest/gtest.h>
 
+
+#ifdef WIN32
+/**
+ * @brief Google tests outputing needs stdout mode to be _O_TEXT
+ * otherwise low level printing will fail.
+ * This is a custom listener to wrapp writing routines
+ */
+struct CustomTestEventListener : public ::testing::EmptyTestEventListener {
+    std::unique_ptr<TestEventListener> mDefault;
+    void OnTestStart(const ::testing::TestInfo& test_info) override {
+        megacmd::WindowsNarrowStdoutGuard smg;
+        mDefault->OnTestStart(test_info);
+    }
+
+    void OnTestPartResult(const ::testing::TestPartResult& result) override {
+        megacmd::WindowsNarrowStdoutGuard smg;
+        mDefault->OnTestPartResult(result);
+    }
+
+    void OnTestEnd(const ::testing::TestInfo& test_info) override {
+        megacmd::WindowsNarrowStdoutGuard smg;
+        mDefault->OnTestEnd(test_info);
+    }
+};
+#endif
+
 int main (int argc, char *argv[])
 {
 #ifdef WIN32
@@ -28,5 +54,15 @@ int main (int argc, char *argv[])
     mega::MegaApi::setLogLevel(mega::MegaApi::LOG_LEVEL_MAX);
 
     testing::InitGoogleTest(&argc, argv);
+
+#ifdef WIN32
+    // Set custom gtests event listener to control the output to stdout
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+    auto customListener = new CustomTestEventListener();
+    customListener->mDefault.reset(listeners.Release(listeners.default_result_printer()));
+    listeners.Append(customListener);
+#endif
+
     return RUN_ALL_TESTS();
 }
