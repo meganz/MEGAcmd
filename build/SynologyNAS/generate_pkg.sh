4e71ebf67a6e08fccf7b66036b0858724c0bb469
Pablo Martin <pm@mega.nz>
2025-02-13 18:39:53 +0100

    minor adjustment in generate_pkg.sh script
    - allow for running from outside megacmd sources
    - allow selecting a different output folder
    - fix cp-ing a second time
    - ensure the image is build (pass --load) to buildx build
    - enruse output folder is created if not existing

--- build/SynologyNAS/generate_pkg.sh
+++ build/SynologyNAS/generate_pkg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <platform>"
@@ -17,14 +19,17 @@ trap cleanup EXIT
 
 echo "Starting build for '${PLATFORM}'..."
 
-BUILDER_PARAMETER="${DOCKER_CUSTOM_BUILDER:+--builder $DOCKER_CUSTOM_BUILDER}"
+BUILDER_PARAMETER="${DOCKER_CUSTOM_BUILDER:+--builder $DOCKER_CUSTOM_BUILDER --load}"
+MEGACMD_FOLDER="${SOURCE_PATH:-${SCRIPT_DIR}/../..}"
+OUTPUT_FOLDER="${OUTPUT_PATH:-$PWD/build/SynologyNAS/packages}"
 
 docker buildx build $BUILDER_PARAMETER -t $CONTAINER \
-  -f "$PWD/build/SynologyNAS/dockerfile/synology-cross-build.dockerfile" \
-  "$PWD" \
+  -f "$SCRIPT_DIR/dockerfile/synology-cross-build.dockerfile" \
+  "${MEGACMD_FOLDER}" \
   --build-arg PLATFORM="$PLATFORM"
 
 CONTAINER_ID=$(docker create $CONTAINER)
-docker cp "${CONTAINER_ID}:/image/${PLATFORM}" $PWD/build/SynologyNAS/packages/$PLATFORM
+mkdir -p "$OUTPUT_FOLDER"
+docker cp "${CONTAINER_ID}:/image/${PLATFORM}" "${OUTPUT_FOLDER}"/
 
 echo "Extracted package for '${PLATFORM}'"
