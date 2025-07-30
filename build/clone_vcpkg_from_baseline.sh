332d000531e155bf8e28029d31790797664cdf31
Pablo Martin <pm@mega.nz>
2025-07-30 11:18:09 +0200

    building adjustments to match newer SDKs
    - Adjust USE_MEDIAINFO => ENABLE_MEDIA_FILE_METADATA
    - update vcpkg.json to match last SDKs updates (including zstd payload
    support)
    - update shallow since commit in vcpkg clone to previous SDK baseline to
    reach required past point.

--- build/clone_vcpkg_from_baseline.sh
+++ build/clone_vcpkg_from_baseline.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-#If a dependency required an older commit, set it here:
-#VCPKG_OLDEST_COMMIT=c6d3ab273572019ea0d6f6688e1163143190d326
+#If a dependency required an older commit (older than baseline), set it here:
+VCPKG_OLDEST_COMMIT=d5ec528843d29e3a52d745a64b469f810b2cedbf
 
 if [ -z ${VCPKG_OLDEST_COMMIT+x} ]; then
     commit=$(grep '."builtin-baseline"' vcpkg.json  | awk -F '"' '{print $(NF-1)}')
